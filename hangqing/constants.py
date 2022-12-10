0bdecf2dec13fa96b25fab5096a7a76e969354a0
Kevin <kmia8694@uni.sydney.edu.au>
2022-12-11 02:25:00 +1100

    调整数据接口演示

--- hangqing/constants.py
+++ hangqing/constants.py
@@ -1,13 +1,29 @@
-import os, math
+import os, math, time
 
 # Cosntants used for API functionalities, do not change
 
 API_TOKEN = "e2faec93d69c35b1a9d348ca46fcf38726370474"
 
 #CML格式代码
-def breaker():
+def breaker(content=" PKU TRADER "):
     """自动格式代码，适应CML宽度
     """
     term_size = os.get_terminal_size().columns
-    half = math.floor((term_size-12)/2)
-    print("\n" + '─' * half + " PKU TRADER " + '─' * half + "\n")
+    
+    if content == " PKU TRADER ":
+        half = math.floor((term_size-12)/2)
+        print("\n" + '─' * half + content + '─' * half + "\n")
+    else:
+        half = math.floor((term_size-len(content))/2)
+        print("\n" + '─' * half + content + '─' * half + "\n")
+
+def wait_t(n=0.5):
+    time.sleep(n)
+    
+def qing():
+    # for windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+ 
+    else:
+        _ = os.system('clear')
--- hangqing/data_strategy_demo.py
+++ hangqing/data_strategy_demo.py
@@ -1,4 +1,4 @@
-from constants import API_TOKEN, breaker
+from constants import API_TOKEN, breaker, wait_t, qing
 from datetime import timedelta
 from tu_data import *
 import pandas as pd
@@ -11,19 +11,23 @@ if __name__ == "__main__":
     
     #示例 - 实时数据
     breaker()
+    print("示例 - 实时数据")
     data = live_data("600519", 1)
     print(data.to_string(index = False))
     
     #示例 - 数据分割方法
     breaker()
+    print("示例 - 数据分割方法")
     print(data[['code', 'price']])
         
-    #示例 - 历史数据(至多一年半)
-    breaker()
-    print(historical_k_data("600519", "2020-1-1"))
+    #示例 - 历史数据(20年) Tushare 暂停维护
+    # breaker()
+    # print("示例 - 历史数据(20年)")
+    # print(historical_k_data("600519", "2020-1-1"))
     
-    #示例 - 历史数据(日线至多一年半，小时数据最多350个历史小时)
+    #示例 - 历史数据(一年半)
     breaker()
+    print("示例 - 历史数据(一年半)")
     df = historical_data("600519", 0)
     df.index = pd.to_datetime(df.index)
     
@@ -34,7 +38,7 @@ if __name__ == "__main__":
     # df = df['2021':'2022']
     
     # 若开盘价高于前收盘价 3%
-    print("\n条件: 开盘价高于收盘价 3%")
+    print("\n历史数据条件筛选: 开盘价高于收盘价 3%")
     op3cl = df.loc[(df["open"] - df["close"].shift(-1))/df["close"].shift(-1) > 0.03]
     
     if op3cl.empty:
@@ -42,23 +46,30 @@ if __name__ == "__main__":
     else:
         print(op3cl)
     
+    
+    
     # 若收盘价高于开盘价
-    print("\n条件: 收盘价高于开盘价 5%")
+    print("\n历史数据条件筛选: 收盘价高于开盘价 5%")
     cl5op = df.loc[(df['close'] - df['open'])/df['open'] > 0.05]
     
     if cl5op.empty:
         print("\n没有匹配的结果\n")
     else:
         print(cl5op)
-        
+    
+    wait_t()
+    
     # 若收盘价低于于开盘价
-    print("\n条件: 收盘价低于开盘价 5%")
+    print("\n历史数据条件筛选: 收盘价低于开盘价 5%")
     cln5op = df.loc[(df['close'] - df['open'])/df['open'] < -0.05]
     
     if cln5op.empty:
         print("\n没有匹配的结果\n")
     else:
         print(cln5op)
+        
+    wait_t(2)
+    qing()
 
     # 简单超买策略回测演示
     print("\n策略: 若收盘价高于开盘价 5% 则融券卖出")
@@ -88,6 +99,8 @@ if __name__ == "__main__":
             print("本次交易收益[${:.2f}]\n".format(profit))
         else:
             print("本次交易损失[${:.2f}]\n".format(profit))
+            
+        wait_t()
 
     breaker()
     print("回测结束")
@@ -95,6 +108,9 @@ if __name__ == "__main__":
     
     print("总体PNL [${:.2f}]".format(pnl_counter))
     print("收益率 [{:.2f}%]".format(pnl_counter/1000000))
+    
+    wait_t(2)
+    qing()
     
     breaker()
     
@@ -126,6 +142,8 @@ if __name__ == "__main__":
             print("本次交易收益[${:.2f}]\n".format(profit))
         else:
             print("本次交易损失[${:.2f}]\n".format(profit))
+            
+        wait_t()
 
     breaker()
     print("回测结束")
